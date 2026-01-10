b104425b8283ea1c51fda7a28101717f8e05cb96
popoves <evgenypopov1998@yandex.ru>
2026-01-10 08:22:18 +0300

    test

--- src/test/java/junit/FirstJunitTest.java
+++ src/test/java/junit/FirstJunitTest.java
@@ -1,5 +1,3 @@
-package junit;
-
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -9,5 +7,10 @@ public class FirstJunitTest {
     @Test
     void successfulSearchTests() {
         open("https://school.qa.guru/cms/system/login?required=true");
+        $("[name=email]").setValue("qagurubot@gmail.com");
+        $("[name=password]").setValue("qagurupassword");
+        $(".btn-success").click();
+        $(".main-header__login").click();
+        $(".logined-form").shouldHave(text("QA_GURU_BOT"));
     }
 }
