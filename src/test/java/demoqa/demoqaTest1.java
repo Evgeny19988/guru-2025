4c4c00de97b94a6e20c71900feb734f3acd1d461
popoves <evgenypopov1998@yandex.ru>
2026-01-10 12:42:26 +0300

    test

--- src/test/java/demoqa/demoqaTest1.java
+++ src/test/java/demoqa/demoqaTest1.java
@@ -17,7 +17,6 @@ public class demoqaTest1 {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
-
     @Test
     void name() {
         open("/text-box");
